f1ead0be4a2ccfb6c09985b9168a27da5fb010ce
Clayton Drown <Crd@drownfamily.com>
2025-11-09 18:34:29 -0500

    flask routes, google api, and basic frountend for testing

--- config.py
+++ config.py
@@ -6,3 +6,21 @@ load_dotenv()
 
 class Config:
     """Base configuration class"""
+
+    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+
+class DebugConfig(Config):
+    """Debug config subclass"""
+
+    DEBUG = True
+    HOST = "0.0.0.0"
+    PORT = 5000
+
+
+class ProductionConfig(Config):
+    """Production config subclass"""
+
+    DEBUG = False
+    HOST = "WSGI HOST"
+    PORT = "PORT"
